bf15213ae1900e277d016356eb7a07a679f42a66
Kaushik <kaushik.2025@vitstudent.ac.in>
2026-02-07 17:21:58 +0530

    added image api route

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from api.v1.iot import IdentifyUserView, DisposeWasteView
+from api.v1.users import UserFaceView
+
 
 urlpatterns = [
     path("v1/iot/identify/", IdentifyUserView.as_view()),
     path("v1/iot/dispose/", DisposeWasteView.as_view()),
+    path("v1/users/<int:user_id>/face/", UserFaceView.as_view()),
+
 ]
