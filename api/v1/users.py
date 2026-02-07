e8760380c5efb996a2b37aae4bc6c3fc69dbaecf
Kaushik <kaushik.2025@vitstudent.ac.in>
2026-02-07 17:20:39 +0530

    Create users.py
    to get user profile pics from website

---
+++ api/v1/users.py
@@ -0,0 +1,23 @@
+from django.http import FileResponse, Http404
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+User = get_user_model()
+
+class UserFaceView(APIView):
+    permission_classes = [IsAuthenticated] 
+
+    def get(self, request, user_id):
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            raise Http404("User not found")
+
+        if not user.profile_photo:
+            raise Http404("User has no profile photo")
+
+        return FileResponse(
+            user.profile_photo.open("rb"),
+            content_type="image/jpeg"
+        )
