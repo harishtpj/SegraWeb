ab09a7f876589a100712ae520347466401b18919
Kaushik <kaushik.2025@vitstudent.ac.in>
2026-02-08 14:16:50 +0530

    added a new serializer for listing user names

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from accounts.models import User
 
 class IdentifySerializer(serializers.Serializer):
     face_encoding = serializers.CharField()
@@ -7,3 +8,11 @@ class DisposeSerializer(serializers.Serializer):
     user_id = serializers.IntegerField()
     waste_type = serializers.CharField()
     weight = serializers.FloatField()
+    
+class UserListSerializer(serializers.ModelSerializer):
+    has_photo = serializers.SerializerMethodField()
+    class Meta:
+        model = User
+        fields = ["username", "has_photo"]
+    def get_has_photo(self, obj):
+        return bool(obj.profile_photo)
