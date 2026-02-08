3f9678bb22814d2bf774a195d80cab8477d2b37f
Kaushik <kaushik.2025@vitstudent.ac.in>
2026-02-08 14:20:47 +0530

    added endpoint for list of users

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from api.v1.iot import IdentifyUserView, DisposeWasteView
 from api.v1.users import UserFaceView
-
+from django.urls import path
+from api.v1.users import UserListView
 
 urlpatterns = [
     path("v1/iot/identify/", IdentifyUserView.as_view()),
     path("v1/iot/dispose/", DisposeWasteView.as_view()),
     path("v1/users/<int:user_id>/face/", UserFaceView.as_view()),
+    path("v1/users/", UserListView.as_view()),
 
 ]
